4d57dab198ccd39fc8f5d0809afd2263a67fd4ec
Thomas Ross <thomasross3610@gmail.com>
2019-09-02 15:22:13 +0100

    form added to allow name input

--- app.rb
+++ app.rb
@@ -4,7 +4,13 @@ get "/" do
   p 'Hello, World!'
 end
 
-get '/cat' do
+get '/random_cat' do
   @name = %w(Amigo Oscar Viking).sample
+  erb(:index)
+end
+
+get '/named_cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
