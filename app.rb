328fd5ac59ec02886a9cac0fca9d9d64927819a3
Thomas Ross <thomasross3610@gmail.com>
2019-09-03 13:27:40 +0100

    end of day 1

--- app.rb
+++ app.rb
@@ -1,12 +1,25 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get "/" do
   p 'Hello, World!'
 end
 
 get '/random_cat' do
-  @name = %w(Amigo Oscar Viking).sample
+  if params[:name] == nil
+    @name = %w(Amigo Oscar Viking).sample
+  elsif params[:name] == ""
+    @name = "Put a fucking name in, mother fucker!"
+  else @name = params[:name]
+  end
   erb(:index)
+end
+
+get '/named_cat' do
+  p params
+  @name = params[:name]
+  erb :index
 end
 
 post '/named_cat' do
@@ -17,4 +30,8 @@ end
 
 get '/cat-form' do
   erb :cat_form
+end
+
+get '/secret-page' do
+  p 'This is a secreeeeeeeet page'
 end
